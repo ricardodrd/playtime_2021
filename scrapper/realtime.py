1dd2d1acb5cfba96e45bbf9577af1b2d97490d2d
ricardodrd <rdricardo.96@gmai.com>
2021-03-19 20:14:11 +0100

    new real time

--- scrapper/realtime.py
+++ scrapper/realtime.py
@@ -16,7 +16,7 @@ def scrap_data_euronext(url):
 
     driver = webdriver.Chrome(executable_path='chromedriver', options=options)
     driver.get(url)
-    sleep(1)
+    sleep(3)
     html = driver.page_source
 
     soup = BeautifulSoup(html, 'lxml')
@@ -46,30 +46,76 @@ def get_both_real_data(symbol, isin, step):
     last_price = float(scrap_data_yahoo(url_yahoo))
     current_price = last_price
     alarm = False
+    latest = 'euronext'
     ser = serial.Serial(port='/dev/ttyACM0', baudrate=9600, timeout=.1)
 
     while True:
-        current_price = float(scrap_data_yahoo(url_yahoo))
-        print('yahoo:',current_price)
-        alarm = evaluate_price(current_price, last_price, alarm, ser)
-        sleep(step/2)
-        current_price = float(scrap_data_euronext(url_euronext))
-        print('euronext:', current_price)
-        alarm = evaluate_price(current_price, last_price, alarm, ser)
+        yahoos_current_price = float(scrap_data_yahoo(url_yahoo))
+        sleep(1)
+        euronex_current_price = float(scrap_data_euronext(url_euronext))
+        print('yahoo:',yahoos_current_price,'euronext:', euronex_current_price)
+        # alarm, current_price, latest = evaluate_price(yahoos_current_price, euronex_current_price, latest, last_price, alarm, ser)
+        alarm = evaluate_price(yahoos_current_price, last_price, alarm, ser)
+        last_price = yahoos_current_price
+        print('Latest price',last_price)
         sleep(step/2)
 
 
-def evaluate_price(current_price, last_price, alarm, ser):
+def evaluate_price(current_yahoo, last_price, alarm, ser):
     status = alarm
-    if ((current_price > last_price) and alarm):
+    # if (current_yahoo != current_euronext):
+    #     if (current_yahoo != last_price and current_euronext != last_price):
+    #         current_price = current_euronext
+    #         latest = 'euronext'
+    #     else:
+    #         if latest == 'euronext':
+    #             if(current_euronext != last_price):
+    #                 current_price = current_yahoo
+    #                 latest = 'yahoo'
+    #             else: 
+    #                 current_price = current_euronext
+    #         else:
+    #             if(current_euronext != last_price):
+    #                 current_price = current_euronext
+    #                 latest = 'euroext'
+    #             else:
+    #                 current_price = current_yahoo
+    # else:
+    #     current_price = current_euronext
+
+    if ((current_yahoo > last_price) and alarm):
         ser.write('n'.encode())
-        print('turn off light')
+        print('Price goes up. Turn off light')
         status = False
-    if ((current_price < last_price) and not alarm):
-        print("turn on light")
+    if ((current_yahoo < last_price) and not alarm):
+        print("Price goes down. Turn on light")
         ser.write('l'.encode())
         status = True
+    
     return status
+
+def scrap_google(search):
+    options = webdriver.ChromeOptions()
+    binary = '/usr/bin/chromium-browser'
+    options.add_argument('headless')
+    options.binary_location = binary 
+
+    driver = webdriver.Chrome(executable_path='chromedriver', options=options)
+    month = '2'
+    day = '1'
+    year = '2021'
+    url = f'https://www.google.com/search?q={search}&biw=857&bih=985&sxsrf=ALeKk00U1s7feq1FWK1tSyHvfndjc1euZw%3A1616123558203&source=lnt&tbs=cdr%3A1%2Ccd_min%3A{month}%2F{day}%2F{year}%2Ccd_max%3A{month}%2F{day}%2F{year}&tbm=nws'
+    driver.get(url)
+
+    html = driver.page_source
+
+    soup = BeautifulSoup(html, 'lxml')
+    soup = soup.find_all('div', {"class":'JheGif nDgy9d'})
+    for header in soup:
+        title = header.text
+        print(title)
+        print()
+
 
 def get_rt_data_euronext(isin_code, step):
     url = f'https://live.euronext.com/en/product/equities/NO0010776990-MERK/market-information'
@@ -85,4 +131,5 @@ if __name__ == "__main__":
     args = parser.parse_args()
     # get_rt_data_euronext(args.symbol, args.step)
     get_both_real_data(args.symbol, args.isin, args.step)
+    # scrap_google(args.symbol)
     # get_rt_data_yahoo(args.symbol, args.step)
