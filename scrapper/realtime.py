a3341b6c24ffdd7d3e56ed5b8a6b1c70dca664e2
ricardodrd <rdricardo.96@gmai.com>
2021-03-19 03:31:57 +0100

    led stock

--- scrapper/realtime.py
+++ scrapper/realtime.py
@@ -3,6 +3,7 @@ import argparse
 from time import sleep
 from bs4 import BeautifulSoup
 from selenium import webdriver
+import serial
 
 # CODES = {"yahoo": '', "euronext": 'NO0010776990'}
 
@@ -13,8 +14,9 @@ def scrap_data_euronext(url):
     options.add_argument('headless')
     options.binary_location = binary 
 
-    driver = webdriver.Chrome(executable_path='chromedriver', chrome_options=options)
+    driver = webdriver.Chrome(executable_path='chromedriver', options=options)
     driver.get(url)
+    sleep(1)
     html = driver.page_source
 
     soup = BeautifulSoup(html, 'lxml')
@@ -26,26 +28,48 @@ def scrap_data_yahoo(url):
     r = requests.get(url)
     soup = BeautifulSoup(r.text, 'lxml')
     soup = soup.find('div', {"class":'My(6px) Pos(r) smartphone_Mt(6px)'})
-    price = soup.find('span').text
+    price = soup.find_all('span')[0].text
+    change = soup.find_all('span')[1].text
+    bid_change = change.split(' ')[0]
     return price
 
 def get_rt_data_yahoo(stock, step):
     # step is the number of seconds to wait between calls
     url = f'https://finance.yahoo.com/quote/{stock}?p={stock}&.tsrc=fin-srch'
-    print(url)
     while True:
-        print(scrap_data_yahoo(url))
+        print(scrap_data_yahoo(url)[1])
         sleep(step)
 
 def get_both_real_data(symbol, isin, step):
     url_yahoo = f'https://finance.yahoo.com/quote/{symbol}?p={symbol}&.tsrc=fin-srch'
     url_euronext = f'https://live.euronext.com/en/product/equities/{isin}-MERK/market-information'
+    last_price = float(scrap_data_yahoo(url_yahoo))
+    current_price = last_price
+    alarm = False
+    ser = serial.Serial(port='/dev/ttyACM0', baudrate=9600, timeout=.1)
+
     while True:
-        print(scrap_data_yahoo(url_yahoo))
+        current_price = float(scrap_data_yahoo(url_yahoo))
+        print('yahoo:',current_price)
+        alarm = evaluate_price(current_price, last_price, alarm, ser)
         sleep(step/2)
-        print(scrap_data_euronext(url_euronext))
+        current_price = float(scrap_data_euronext(url_euronext))
+        print('euronext:', current_price)
+        alarm = evaluate_price(current_price, last_price, alarm, ser)
         sleep(step/2)
 
+
+def evaluate_price(current_price, last_price, alarm, ser):
+    status = alarm
+    if ((current_price > last_price) and alarm):
+        ser.write('n'.encode())
+        print('turn off light')
+        status = False
+    if ((current_price < last_price) and not alarm):
+        print("turn on light")
+        ser.write('l'.encode())
+        status = True
+    return status
 
 def get_rt_data_euronext(isin_code, step):
     url = f'https://live.euronext.com/en/product/equities/NO0010776990-MERK/market-information'
@@ -60,4 +84,5 @@ if __name__ == "__main__":
     parser.add_argument("-s", "--step", help="symbol letters of the company", type=int, default=20)
     args = parser.parse_args()
     # get_rt_data_euronext(args.symbol, args.step)
-    get_both_real_data(args.symbol, args.isin, args.step)    
+    get_both_real_data(args.symbol, args.isin, args.step)
+    # get_rt_data_yahoo(args.symbol, args.step)
--- serial_communication.py
+++ serial_communication.py
@@ -1,15 +1,34 @@
 import serial
+from time import sleep
 
 def main():
-    while True:
-        ser = serial.Serial(port='/dev/ttyACM0', baudrate=9600, timeout=.1)
-        user_input = input("Enter something:")
-        if user_input == "a":
-            ser.write('a'.encode())
-        if user_input == "s":
-            ser.write('s'.encode())
-        if user_input == "quit":
-            break
+    current_prices = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+    last_prices = [1,4,3,3,5,6,7,8,9,10,11,12,13,14,15]
+
+    alarm = False
+    ser = serial.Serial(port='/dev/ttyACM0', baudrate=9600, timeout=.1)
+
+    for idx in range(0,len(current_prices)):
+        print("current:",current_prices[idx],", last:",last_prices[idx])
+        if ((current_prices[idx] > last_prices[idx]) and alarm):
+            ser.write('n'.encode())
+            print('turn off light')
+            alarm = False
+        if ((current_prices[idx] < last_prices[idx]) and not alarm):
+            ser.write('l'.encode())
+            print("turn on light")
+            alarm = True
+        sleep(2)
+
+    # while True:
+    #     ser = serial.Serial(port='/dev/ttyACM0', baudrate=9600, timeout=.1)
+    #     user_input = input("Enter something:")
+    #     if user_input == "a":
+    #         ser.write('a'.encode())
+    #     if user_input == "s":
+    #         ser.write('s'.encode())
+    #     if user_input == "quit":
+    #         break
 
 if __name__ == '__main__':
     main()
