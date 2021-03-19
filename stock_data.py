ee89e14b11fae8afc4cdc95a4f980e5ea3f74556
ricardodrd <rdricardo.96@gmai.com>
2021-03-19 19:08:58 +0100

    done

--- stock_data.py
+++ stock_data.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 import pandas_datareader as web
 import numpy as np
 import argparse
+from bs4 import BeautifulSoup
+from time import sleep
+from selenium import webdriver
 
 def compare_stocks(main_company, companies):
     start_date = datetime.datetime(2000, 1,1)
@@ -23,6 +26,48 @@ def compare_stocks(main_company, companies):
     plt.ylabel('Volume')
     plt.xlabel('Days')
     plt.show()
+
+def scrap_lowest(symbol, company_name):
+    start_date = datetime.datetime(2015, 1,1)
+    end_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    stock_df = web.DataReader(symbol, 'yahoo', start_date, end_date)
+    stock_df['difference_crash'] = stock_df['Open'] - stock_df['Close']
+    stock_df['difference_rise'] = stock_df['Close'] - stock_df['Open']
+    row_crash = stock_df[stock_df['difference_crash']==stock_df['difference_crash'].max()]
+    crash_date = row_crash.index.date[0]
+    print(f'WORST DAY ({crash_date})')
+    print("------------------------------------------------")
+    scrap_google(company_name, crash_date)
+    print("------------------------------------------------")
+    row_rise = stock_df[stock_df['difference_rise']==stock_df['difference_rise'].max()]
+    rise_date = row_rise.index.date[0]
+    print(f'BEST DAY ({rise_date})')
+    print("------------------------------------------------")
+    scrap_google(company_name, rise_date)
+    print("------------------------------------------------")
+
+
+def scrap_google(search, search_date):
+    options = webdriver.ChromeOptions()
+    binary = '/usr/bin/chromium-browser'
+    options.add_argument('headless')
+    options.binary_location = binary 
+
+    driver = webdriver.Chrome(executable_path='chromedriver', options=options)
+    month = str(search_date.month)
+    day = str(search_date.day)
+    year = str(search_date.year)
+    url = f'https://www.google.com/search?q={search}&biw=857&bih=985&sxsrf=ALeKk00U1s7feq1FWK1tSyHvfndjc1euZw%3A1616123558203&source=lnt&tbs=cdr%3A1%2Ccd_min%3A{month}%2F{day}%2F{year}%2Ccd_max%3A{month}%2F{day}%2F{year}&tbm=nws'
+    driver.get(url)
+    sleep(1)
+    html = driver.page_source
+
+    soup = BeautifulSoup(html, 'lxml')
+    soup = soup.find_all('div', {"class":'JheGif nDgy9d'})
+    for header in soup:
+        title = header.text
+        print(title)
+        print()
 
 def main(stock):
     start_date = datetime.datetime(2021, 1,1)
@@ -51,11 +96,13 @@ def plot_cols(df, cols):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("symbol", help="symbol letters of the company", type=str)
+    parser.add_argument("name", help="name of the company", type=str)
     parser.add_argument("-s", "--start", help="start_date", type=str)
     parser.add_argument("-e", "--end", help="end date", type=str)
     parser.add_argument('-c', '--companies', action='store',
                     type=str, nargs='*', default=None,
                     help="Examples: -i item1 item2, -i item3")
     args = parser.parse_args()
-    main(args.symbol)
+    # main(args.symbol)
+    scrap_lowest(args.symbol, args.name)
     # compare_stocks(args.symbol, args.companies)
